7c439d4302466db6bf668d1af5cf70678fa56833
Hp <Hp@LAPTOP-D9PNIJAT>
2017-03-24 22:12:04 +0000

    stunner
    -hero now stuns the ogres

--- 1LPOO1617/src/character_subclasses/Ogre.java
+++ 1LPOO1617/src/character_subclasses/Ogre.java
@@ -12,6 +12,7 @@ public class Ogre extends Character{
 	public int randomNum;
 	private Random randomGen = new Random();
 	private Club club;
+	private int stunned_status = 0;
 	
 	public Ogre(Game g, int x, int y) {// Basic Constructor
 		
@@ -38,8 +39,9 @@ public class Ogre extends Character{
 		int x = hero.getPosX(), y = hero.getPosY();
 		
 		if(Math.abs(x-this.posx)<=1 && Math.abs(y-this.posy)<=1){
-			System.out.println("The Ogre caught you!");
-			this.g.setState(0);
+			/*System.out.println("The Ogre caught you!");
+			this.g.setState(0);*/
+			stunned_status = 2;
 		}
 	}
 	
@@ -47,30 +49,37 @@ public class Ogre extends Character{
 		
 		this.club.clearClub();
 		
-		while(true){
-			randomNum = randomGen.nextInt(4) + 1;
-			
-			if(randomNum == 1){
-				if(this.moveCharacter('w') == 1)
-					break;
-			}
-			else if(randomNum == 2){
-				if(this.moveCharacter('a') == 1)
-					break;
-			}
-			else if(randomNum == 3){
-				if(this.moveCharacter('s') == 1)
-					break;
-			}
-			else if(randomNum == 4){
-				if(this.moveCharacter('d') == 1)
-					break;
-			}
-			else{
-				System.out.println("Error in ogrePatrol(), invalid randomNum generated!");
-				break;
-			}
+		if(stunned_status != 0){
+			stunned_status--;
+			return;
 		}
+		else{
+			while(true){
+				randomNum = randomGen.nextInt(4) + 1;
+				
+				if(randomNum == 1){
+					if(this.moveCharacter('w') == 1)
+						break;
+				}
+				else if(randomNum == 2){
+					if(this.moveCharacter('a') == 1)
+						break;
+				}
+				else if(randomNum == 3){
+					if(this.moveCharacter('s') == 1)
+						break;
+				}
+				else if(randomNum == 4){
+					if(this.moveCharacter('d') == 1)
+						break;
+				}
+				else{
+					System.out.println("Error in ogrePatrol(), invalid randomNum generated!");
+					break;
+				}
+			}			
+		}
+
 	}
 	
 	public void checkForKey(Lever key){// Changes Ogre to "guarding the key"(" $ ") if on top of it
